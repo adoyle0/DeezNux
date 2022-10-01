8cdfad6ceecd82e252ed4ec8615b0d1b21ae4c0b
Adam <24621027+WhiteDopeOnPunk@users.noreply.github.com>
2022-10-01 01:47:20 -0400

    vim ftw

---
+++ .config/nvim/lua/adam/remap.lua
@@ -0,0 +1,8 @@
+local nnoremap = require("adam.keymap").nnoremap
+
+nnoremap("<leader>pv", "<cmd>Ex<CR>")
+nnoremap("<leader>ps", "<cmd>PackerSync<CR>")
+
+-- does sthis even work?
+nnoremap("<leader>p", "\"_dP")
+
