ce66ca90934c76ef061ce6f1d1fc8ce468e2d44a
Adam <24621027+WhiteDopeOnPunk@users.noreply.github.com>
2022-10-09 16:07:49 -0400

    cmp

--- .config/nvim/lua/adam/packer.lua
+++ .config/nvim/lua/adam/packer.lua
@@ -6,8 +6,17 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
+
   -- Visual
   use 'vim-airline/vim-airline'
+
   -- LSP
   use 'neovim/nvim-lspconfig'
+
+  -- Completion
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
 end)
