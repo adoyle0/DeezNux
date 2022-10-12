14ce3a66599d7a6f6bb56c251553b5bed4185b56
Adam <24621027+WhiteDopeOnPunk@users.noreply.github.com>
2022-10-12 18:08:35 -0400

    add install script/package list

---
+++ .scripts/installdeps.sh
@@ -0,0 +1,45 @@
+#!/bin/zsh
+
+# Install all (most) dependencies
+# This also serves as a list of dependencies
+
+yay -S --needed \
+\
+`# Standard`\
+awesome \
+kitty \
+neovim \
+firefox-developer-edition \
+python-pywal \
+thunderbird \
+discord \
+caprine \
+telegram-desktop \
+godot \
+playerctl \
+pamixer \
+rsync \
+byobu \
+zsh \
+xorg-xmodmap \
+picom \
+unclutter \
+redshift \
+xclip \
+polkit-gnome \
+lua-language-server \
+arc-icon-theme \
+acpi \
+pyright \
+lua-language-server \
+\
+`# Aur`\
+brave-nightly-bin \
+spotify \
+python-pywalfox \
+pywal-discord-git \
+wal-telegram-git \
+lf-git \
+ctpv-git \
+nvim-packer-git \
+vscode-langservers-extracted \
